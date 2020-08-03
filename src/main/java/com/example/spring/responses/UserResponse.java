d520bfe90ce4bd434cf51e4727c1a7ea416a7db5
unknown <moreno7046@hotmail.com>
2020-08-03 03:42:51 -0500

    finished exercises

---
+++ src/main/java/com/example/spring/responses/UserResponse.java
@@ -0,0 +1,48 @@
+package com.example.spring.responses;
+
+import java.util.UUID;
+
+public class UserResponse {
+
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public UserResponse() {
+
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
